cb7a26c6d01e0f2ff4328ad696fa187882d54c18
maru-256 <maru043@docomo.ne.jp>
2021-10-11 21:52:41 +0900

    MovingBlockを実装

--- src/main/java/net/kunmc/lab/koekatamarin/Koekatamarin.java
+++ src/main/java/net/kunmc/lab/koekatamarin/Koekatamarin.java
@@ -1,13 +1,26 @@
 package net.kunmc.lab.koekatamarin;
 
+import dev.kotx.flylib.FlyLib;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public final class Koekatamarin extends JavaPlugin {
+    public static Koekatamarin instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        FlyLib.create(this, builder -> {
+            builder.listen(PlayerInteractEvent.class, e -> {
+                Vector direction = e.getPlayer().getLocation().getDirection();
+                Location location = e.getPlayer().getEyeLocation().add(direction.multiply(1.0));
+                new MovingBlock(location, 6.0, Material.DIAMOND_BLOCK.createBlockData());
+            });
+        });
     }
 
     @Override
---
+++ src/main/java/net/kunmc/lab/koekatamarin/MovingBlock.java
@@ -0,0 +1,83 @@
+package net.kunmc.lab.koekatamarin;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftArmorStand;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.entity.Shulker;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
+
+import static org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason.CUSTOM;
+
+public class MovingBlock {
+    private final Location spawnedLocation;
+    private final ArmorStand armorStand;
+    private final FallingBlock fallingBlock;
+    private final BukkitTask movingTask;
+    private double speedPerSecond;
+
+    public MovingBlock(Location location, double speedPerSecond, BlockData blockData) {
+        this.spawnedLocation = location.clone();
+        this.speedPerSecond = speedPerSecond;
+        World world = location.getWorld();
+
+        armorStand = world.spawn(spawnedLocation, ArmorStand.class, CUSTOM, x -> {
+            x.setMarker(true);
+            x.setVisible(false);
+            x.setGravity(false);
+        });
+
+        world.spawn(spawnedLocation, Shulker.class, CUSTOM, x -> {
+            x.setAI(false);
+            x.setSilent(true);
+            x.setInvulnerable(true);
+            x.setInvisible(true);
+            x.setLootTable(null);
+            armorStand.addPassenger(x);
+        });
+
+        fallingBlock = world.spawnFallingBlock(spawnedLocation, blockData);
+        fallingBlock.setInvulnerable(true);
+        fallingBlock.setDropItem(false);
+        fallingBlock.setGravity(false);
+        armorStand.addPassenger(fallingBlock);
+
+        movingTask = new MovingTask().runTaskTimerAsynchronously(Koekatamarin.instance, 0, 0);
+    }
+
+    public double speedPerSecond() {
+        return speedPerSecond;
+    }
+
+    public void speedPerSecond(double speedPerSecond) {
+        this.speedPerSecond = speedPerSecond;
+    }
+
+    public void remove() {
+        movingTask.cancel();
+        armorStand.getPassengers().forEach(Entity::remove);
+        armorStand.remove();
+    }
+
+    private class MovingTask extends BukkitRunnable {
+        @Override
+        public void run() {
+            fallingBlock.setTicksLived(1);
+
+            Vector direction = spawnedLocation.getDirection();
+            Vector travelDistance = direction.multiply(speedPerSecond / 20);
+            Location next = armorStand.getLocation().add(travelDistance);
+
+            CraftArmorStand craftArmorStand = ((CraftArmorStand) armorStand);
+            try {
+                craftArmorStand.getHandle().teleportAndSync(next.getX(), next.getY(), next.getZ());
+            } catch (Exception ignored) {
+            }
+        }
+    }
+}
